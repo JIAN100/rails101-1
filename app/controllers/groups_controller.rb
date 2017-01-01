b096b5762bc0a381989b4156a6ede01e1c2e6132
jian <xiaoshuxixi@gmail.com>
2017-01-01 19:19:01 +0800

    people can't see edit button unless he is group owner

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,16 +1,15 @@
 class GroupsController < ApplicationController
   before_action :authenticate_user! , only: [:new, :create, :edit, :update, :destroy]
   before_action :find_group_and_check_permission, only: [:edit, :update, :destroy]
-     @groups = Group.all
-   end
+  @groups = Group.all
+
 
    def show
      @group = Group.find(params[:id])
    end
 
    def edit
-    find_group_and_check_permission
-  end
+   end
 
    def new
      @group = Group.new
@@ -29,34 +28,30 @@ class GroupsController < ApplicationController
 
 
 
-   def update
-
-  find_group_and_check_permission
-
-  if @group.update(group_params)
-    redirect_to groups_path, notice: "Update Success"
-  else
-    render :edit
+  def update
+    if @group.update(group_params)
+      redirect_to groups_path, notice: "Update Success"
+    else
+      render :edit
+    end
   end
-end
 
-def destroy
- find_group_and_check_permission
+  def destroy
+      @group.destroy
+      redirect_to groups_path, alert: "Group deleted"
+  end
 
- @group.destroy
- redirect_to groups_path, alert: "Group deleted"
-end
+private
 
-   private
+ def find_group_and_check_permission
+   @group = Group.find(params[:id])
 
-   def find_group_and_check_permission
-       @group = Group.find(params[:id])
+   if current_user != @group.user
+     redirect_to root_path, alert: "You have no permission."
+   end
+  end
 
-       if current_user != @group.user
-         redirect_to root_path, alert: "You have no permission."
-       end
-     end
    def group_params
      params.require(:group).permit(:title, :description)
    end
- end
+end
